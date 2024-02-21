0af029fd688f42ac249fc3f8cb4d90056630a010
Prapti Jaduvanshi <khushi.jaduvanshi@gmail.com>
2024-02-21 19:40:05 +0530

    Sign-In Page

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,6 +1,28 @@
+import Link from "next/link"
+import { Icons } from "./Icons"
+
+//Sign-In is a server component, hence can't have interactivity. 
+//UserAuthForm is used as the client component.
 
 const SignIn = ({}) => {
-  return <div>SignIn</div>
+  return (
+  <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
+    <div className="flex flex-col space-y-2 text-center">
+      <Icons.logo className="mx-auto h-8 w-8"/>
+
+      <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
+      <p className="text-sm max-w-xs mx-auto">By continuing, you are setting up a Dunkit account and agree to our User Agreement and Privacy Policy.</p>
+
+      {/* Sign-In form */}
+
+
+      <p className="px-8 text-center text-sm text-zinc-700">
+        New to Dunkit?{' '}
+        <Link href={'/sign-up'} className="hover:text-zinc-800 text-sm underline underline-offset-4">Sign Up</Link>
+      </p>
+    </div>
+  </div>
+  )
 }
 
 export default SignIn
---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+import { Button } from './ui/Button'
+
+interface UserAuthFormProps {
+  
+}
+
+const UserAuthForm: FC<UserAuthFormProps> = ({}) => {
+  return (
+    <div>
+        <Button>Google</Button>
+    </div>
+  )
+}
+
+export default UserAuthForm
