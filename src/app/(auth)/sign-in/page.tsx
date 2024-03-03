ddf6cf6705ce1db41f70a03eba1532ae2790ba76
Prapti Jaduvanshi <khushi.jaduvanshi@gmail.com>
2024-03-04 00:24:33 +0530

    Modal for SignIn

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,7 @@
 import SignIn from '@/components/SignIn'
 import { buttonVariants } from '@/components/ui/Button'
 import { cn } from '@/lib/utils'
+import { ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
 import { FC } from 'react'
 
@@ -9,7 +10,9 @@ const page: FC = ({}) => {
   <div className='absolute inset-0'>
     <div className='h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20'>
         {/*Navigate back to homepage (applies ghost classnames) */}
-        <Link href='/' className={cn(buttonVariants({variant: 'ghost'}), 'self-start -mt-20')}>Home</Link>
+        <Link href='/' className={cn(buttonVariants({variant: 'ghost'}), 'self-start -mt-20')}>
+          <ChevronLeft className='mr-2 h-4 w-4' /> Home
+        </Link>
 
         <SignIn />
     </div>
---
+++ src/app/@authModal/default.tsx
@@ -0,0 +1,6 @@
+// For intercept/parallel routes
+
+//Determines what should be shown if no routes is able to be intercepted.
+export default function Default() {
+    return null
+}
