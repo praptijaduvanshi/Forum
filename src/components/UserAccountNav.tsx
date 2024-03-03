2e7c80d942b33a8c863d9860526a16aa58c45cc1
Prapti Jaduvanshi <khushi.jaduvanshi@gmail.com>
2024-03-03 23:34:49 +0530

    User profile menu

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,8 +1,12 @@
+'use client'
+
 import { User } from 'next-auth'
 import { userAgent } from 'next/server'
 import { FC } from 'react'
-import { DropdownMenu, DropdownMenuTrigger } from '@/components/ui/DropdownMenu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/DropdownMenu'
 import UserAvatar from '@/components/UserAvatar'
+import Link from 'next/link'
+import { signOut } from 'next-auth/react'
 
 interface UserAccountNavProps {
   user: Pick<User, 'name' | 'image' | 'email'>
@@ -19,6 +23,44 @@ const UserAccountNav: FC<UserAccountNavProps> = ({user}) => {
           image: user.image || null
         }}/>
       </DropdownMenuTrigger>
+
+      {/* When image is clicked */}
+      <DropdownMenuContent className='bg-white' align='end'>
+        <div className='flex items-center justify-start gap-2 p-2'>
+          <div className='flex flex-col space-y-1 leading-none'>
+            {/*If the user has username, display name */}
+            {user.name && <p className='font-medium'>{user.name}</p>}
+            {user.email && <p className='w-[200px] truncate text-sm text-zinc-700'>{user.email}</p>}
+          </div>
+        </div>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href='/'>Feed</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href='/r/create'>Create community</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href='/settings'>Settings</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem onSelect={(event) => {
+          event.preventDefault()
+          signOut({
+            callbackUrl: `${window.location.origin}/sign-in`
+          })
+        }} className='cursor-pointer'>
+          Sign Out
+        </DropdownMenuItem>
+
+      </DropdownMenuContent>
+
     </DropdownMenu>
   )
 }
